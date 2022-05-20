b5bfde2c9a753229ccab93124fc29a873164c6de
Marc Philipp <mail@marcphilipp.de>
2022-05-20 09:50:42 +0200

    Add Gradle Enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+plugins {
+    id("com.gradle.enterprise") version "3.10.1"
+}
+
+gradleEnterprise {
+    server = "https://ge.junit.org"
+    buildScan {
+        publishAlways()
+    }
+}
+
 rootProject.name = "junit-wrapper-upgrade"
