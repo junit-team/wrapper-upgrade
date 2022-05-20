01d3843ea9044dfe6d964a76f189cc3fd3c3b82f
Marc Philipp <marc@gradle.com>
2022-05-20 10:44:09 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    base
+    id("org.gradle.wrapper-upgrade") version "0.10.2"
+}
+
+val gradleSampleProjects = listOf(
+    "junit5-jupiter-extensions",
+    "junit5-jupiter-starter-gradle",
+    "junit5-jupiter-starter-gradle-groovy",
+    "junit5-jupiter-starter-gradle-kotlin",
+    "junit5-migration-gradle",
+    "junit5-multiple-engines"
+)
+
+val mavenSampleProjects = listOf(
+    "junit5-jupiter-starter-maven",
+    "junit5-jupiter-starter-maven-kotlin",
+    "junit5-migration-maven"
+)
+
+val gitCommitArgs = listOf(
+    "--author=JUnit Team <team@junit.org>"
+)
+
+wrapperUpgrade {
+    gradle {
+        gradleSampleProjects.forEach { projectName ->
+            create("${projectName}-sample") {
+                repo.set("junit-team/junit5-samples")
+                dir.set(projectName)
+                options.gitCommitExtraArgs.set(gitCommitArgs)
+            }
+        }
+    }
+    maven {
+        mavenSampleProjects.forEach { projectName ->
+            create("${projectName}-sample") {
+                repo.set("junit-team/junit5-samples")
+                dir.set(projectName)
+                options.gitCommitExtraArgs.set(gitCommitArgs)
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "junit-wrapper-upgrade"
